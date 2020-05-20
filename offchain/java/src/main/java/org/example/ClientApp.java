1b69e011e3ee9229f6a7480aa8d4f73dc5028b55
Krzysztof Nowakowski <ext-krzysztof.nowakowski@finago.com>
2020-05-20 18:48:35 +0200

    Add kafka support

--- offchain/java/src/main/java/org/example/ClientApp.java
+++ offchain/java/src/main/java/org/example/ClientApp.java
@@ -1,5 +1,14 @@
 package org.example;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.hyperledger.fabric.gateway.Contract;
 import org.hyperledger.fabric.gateway.Gateway;
 import org.hyperledger.fabric.gateway.Network;
@@ -7,47 +16,86 @@ import org.hyperledger.fabric.gateway.Wallet;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
 
 public class ClientApp {
 	
-	private static String user = "user6";
+	private static final String USER = "user";
+	private static final String CHANNEL = "mychannel";
+	private static final String CONTRACT = "imagedb";
+	public static final String KAFKA_BOOTSTRAP = "localhost:9092";
+	public static final String OUTPUT_TOPIC = "output-topic";
+	public static final String REQUEST_DATA_TOPIC = "request-data-topic";
+	public static final String RESULT_DATA_TOPIC = "result-data-topic";
+	private static final KafkaProducer<String, String> producer = new KafkaProducer<>(getProducerProperties());
+	private static final KafkaConsumer<String, String> consumer = new KafkaConsumer<>(getConsumerProperties());
 	
 	static {
 		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
 		try {
 			EnrollAdmin.enrollAdmin();
-			RegisterUser.registerUser(user);
+			RegisterUser.registerUser(USER);
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new ExceptionInInitializerError(e);
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
+		consumer.subscribe(List.of(OUTPUT_TOPIC, REQUEST_DATA_TOPIC));
+		
 		Path walletPath = Paths.get("wallet");
 		Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 		Path networkConfigPath = Paths.get(ClassLoader.getSystemResource("connection.yaml").toURI());
 		
 		Gateway.Builder builder = Gateway.createBuilder();
-		builder.identity(wallet, user).networkConfig(networkConfigPath).discovery(true);
+		builder.identity(wallet, USER).networkConfig(networkConfigPath).discovery(true);
 		
 		try (Gateway gateway = builder.connect()) {
 			
-			Network network = gateway.getNetwork("mychannel");
-			Contract contract = network.getContract("image");
+			Network network = gateway.getNetwork(CHANNEL);
+			Contract contract = network.getContract(CONTRACT);
 			
-			byte[] result;
-			
-			final byte[] addedImage = contract.submitTransaction("addImage", "1", "blabla--");
-			System.out.println("ADDED: " + new String(addedImage));
-
-//			result = contract.evaluateTransaction("getImagesBetweenDates", "0", "1");
-//			System.out.println(new String(result));
-//			
-			result = contract.evaluateTransaction("getImages");
-			System.out.println(new String(result));
-			
-			result = contract.evaluateTransaction("getImageByMillis", "1");
-			System.out.println(new String(result));
+			while (true) {
+				final ConsumerRecords<String, String> consumerRecords =
+						consumer.poll(Duration.ofMillis(1000));
+				
+				for (ConsumerRecord<String, String> record : consumerRecords) {
+					if (OUTPUT_TOPIC.equals(record.topic())) {
+						contract.submitTransaction("addImage", String.valueOf(System.currentTimeMillis()), record.value());
+					}
+					
+					if (REQUEST_DATA_TOPIC.equals(record.topic())) {
+						final String[] bounds = record.value().split(" ");
+						long startTime = Long.parseLong(bounds[0].replaceAll("[^\\d.]", ""));
+						long endTime = Long.parseLong(bounds[1].replaceAll("[^\\d.]", ""));
+						byte[] result = contract.evaluateTransaction("getImagesBetweenDates", String.valueOf(startTime), String.valueOf(endTime));
+						producer.send(new ProducerRecord<>(RESULT_DATA_TOPIC, new String(result)));
+					}
+				}
+				consumer.commitAsync();
+			}
 		}
+	}
+	
+	private static Properties getConsumerProperties() {
+		Properties consumerProps = new Properties();
+		consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP);
+		consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+		consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+		consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return consumerProps;
+	}
+	
+	private static Properties getProducerProperties() {
+		Properties producerProps = new Properties();
+		producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP);
+		producerProps.put(ProducerConfig.ACKS_CONFIG, "all");
+		producerProps.put(ProducerConfig.RETRIES_CONFIG, 0);
+		producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return producerProps;
 	}
 }
